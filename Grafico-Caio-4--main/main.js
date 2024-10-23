a688db8cf702f1b90925a14be2cc331e4bc11041
CaioTavares2007 <162310979+CaioTavares2007@users.noreply.github.com>
2024-10-23 11:05:20 -0300

    Add files via upload

---
+++ Grafico-Caio-4--main/main.js
@@ -0,0 +1,22 @@
+const grafico = document.getElementById("meuGrafico").getContext("2d");
+const meuGrafico = new Chart (grafico,{
+type:"pie",
+data: {
+ labels:[
+"Instagram",
+"WhatsApp",
+"Facebook",
+"WeChat",
+"TikTok",
+"DouYin",
+"Twitter",
+"Telegram",
+"FB Messenger",
+"LINE"
+ ],
+
+
+
+}
+}
+)
